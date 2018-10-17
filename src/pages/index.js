93d5988845b64e3a7f26f4a7b248079f88cb6144
Brian Andrews <sbardian@gmail.com>
2018-10-17 12:52:56 -0600

    Index page build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { css } from 'emotion';
 import 'normalize.css';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
@@ -8,12 +9,44 @@ class IndexPage extends Component {
     const { owners } = this.props.data.site.siteMetadata;
     return (
       <Layout>
-        <h1>Select wish list:</h1>
-        {owners.map(owner => (
-          <div key={owner.name}>
-            <Link to={owner.name}>{owner.name}</Link>
-          </div>
-        ))}
+        <h1
+          className={css`
+            display: grid;
+            justify-content: center;
+            color: #666;
+          `}
+        >
+          Select wish list
+        </h1>
+        <div
+          className={css`
+            display: grid;
+            grid-gap: 20px;
+            grid-template-columns: repeat(auto-fill, minmax(150px, 250px));
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          {owners.map(owner => (
+            <Link key={owner.name} to={owner.name}>
+              <div
+                className={css`
+                  height: 200px;
+                  background-color: #b39c82;
+                  font-size: 38pt;
+                  color: #e1e1e1;
+                  display: grid;
+                  justify-content: center;
+                  align-items: center;
+                  text-transform: uppercase;
+                  text-rendering: optimizeLegibility;
+                `}
+              >
+                {owner.name}
+              </div>
+            </Link>
+          ))}
+        </div>
       </Layout>
     );
   }
