b9d217032c0aa88264187d59af5aabbe31f8ebe5
Brian Andrews <sbardian@gmail.com>
2018-10-17 15:22:54 -0600

    Fix background image to cover

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'emotion';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
@@ -29,14 +30,34 @@ const Layout = ({ children }) => (
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
+          className={css`
+            margin: 0 auto;
+            max-width: 960px;
+            &::after {
+              content: '';
+              background-image: url('http://www.alerjidernegi.org.tr/wp-content/uploads/2016/04/shopping-bags-820x510-728x453.jpg');
+              background-repeat: no-repeat;
+              background-size: cover;
+              background-attachment: fixed;
+              background-position: center;
+              position: absolute;
+              top: 0;
+              left: 0;
+              bottom: 0;
+              right: 0;
+              z-index: -1;
+            }
+          `}
         >
-          {children}
+          <div
+            className={css`
+              background-color: #b8c99d;
+              margin: 20px;
+              padding: 20px;
+            `}
+          >
+            {children}
+          </div>
         </div>
       </>
     )}
