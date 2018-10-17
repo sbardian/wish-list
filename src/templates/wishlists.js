17641f9098d9857c3f9330fde3b38922dec85d86
Brian <sbardian@gmail.com>
2018-10-17 17:02:31 -0600

    Add data to wishlist template

--- src/templates/wishlists.js
+++ src/templates/wishlists.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { css } from 'emotion';
 import Layout from '../components/Layout';
 
 export default ({ data }) => {
   return (
     <Layout>
       <div>
-        <h1>hey</h1>
+        <h1
+        >{`${data.allAmazonWishlistItem.edges[0].node.owner.toUpperCase()}'s wish list`}</h1>
         {data.allAmazonWishlistItem.edges.map(item => (
-          <div key={item.node.id}>
-            <span>{item.node.title}</span>
+          <div
+            key={item.node.id}
+            className={css`
+              padding: 20px;
+              border-bottom: 1px solid #666;
+            `}
+          >
+            <span>
+              <a href={item.node.url}>{item.node.title}</a>
+            </span>
+            <div>
+              <img src={item.node.image.url} alt={item.node.title} />
+            </div>
+            <div>
+              <span>{item.node.price}</span>
+            </div>
+            <div>
+              <span>{item.node.features}</span>
+            </div>
+            <div>
+              <span>{item.node.priority}</span>
+            </div>
+            <div>
+              <span>qty: {item.node.requested}</span>
+            </div>
           </div>
         ))}
       </div>
