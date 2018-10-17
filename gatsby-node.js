3b6eebf25072e54bc55c703dde7f9f5e358b9701
Brian Andrews <sbardian@gmail.com>
2018-10-17 09:12:14 -0600

    Update to build pages based on config options and pass owner in context

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,26 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
 const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` });
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    });
-  }
-};
-
-/** TODO: query and pass owner names from gatsby-config, or some other
- * config file.  Then generate pages based on owner name using it as
- * information passed in the context to each page to be used in the query
- * as a variable to filter the node items pertaining to that owner.
- */
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return new Promise((resolve, reject) => {
     graphql(`
       {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
+        site {
+          siteMetadata {
+            owners {
+              name
             }
           }
         }
       }
     `).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      result.data.site.siteMetadata.owners.forEach(owner => {
         createPage({
-          path: node.fields.slug,
+          path: owner.name,
           component: path.resolve(`./src/templates/wishlists.js`),
           context: {
-            // Data passed to context is available
-            // in page queries as GraphQL variables.
-            slug: node.fields.slug,
+            slug: owner.name,
+            owner: owner.name,
           },
         });
       });
