2ecdb0c5fcea9c733f6d9a56c32fe23dfd3345b0
Brian Andrews <sbardian@gmail.com>
2018-10-18 12:23:40 -0600

    Add background color to body

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,9 @@ import Typography from 'typography';
 import lincolnTheme from 'typography-theme-lincoln';
 
 lincolnTheme.overrideThemeStyles = ({ rhythm }, options) => ({
+  body: {
+    backgroundColor: '#e2e2e2',
+  },
   a: {
     textShadow: 'none',
     backgroundImage: 'none',
